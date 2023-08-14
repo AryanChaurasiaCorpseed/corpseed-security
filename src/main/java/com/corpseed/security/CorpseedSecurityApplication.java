1453abf493f8e4cf87b3b3ee0230b2eef3fa3401
corpseedBackend <ajay.kumar@corpseed.com>
2023-08-14 16:27:02 +0530

    implement security

--- src/main/java/com/corpseed/security/CorpseedSecurityApplication.java
+++ src/main/java/com/corpseed/security/CorpseedSecurityApplication.java
@@ -2,8 +2,8 @@ package com.corpseed.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+
 public class CorpseedSecurityApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/corpseed/security/payload/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.corpseed.security.payload.response;
+
+public class MessageResponse {
+ 
+	String message;
+	public MessageResponse(String ms) {
+		// TODO Auto-generated constructor stub
+		this.message=ms;
+	}
+
+	
+}
