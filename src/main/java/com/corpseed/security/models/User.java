23cf5efd38c2c873f1c8e72743eabff62ead3c8b
corpseedBackend <ajay.kumar@corpseed.com>
2023-10-09 15:49:55 +0530

    resolved issue

--- src/main/java/com/corpseed/security/models/User.java
+++ src/main/java/com/corpseed/security/models/User.java
@@ -18,29 +18,21 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-
   private String username;
-
-
   private String email;
-
-
   private String password;
 
   @ManyToMany(fetch = FetchType.LAZY)
   @JoinTable(  name = "user_roles", 
         joinColumns = @JoinColumn(name = "user_id"), 
         inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private List<Role> roles = new ArrayList<>();
-  
-  String companyName;
-  
+  private List<Role> roles = new ArrayList<>();  
+  String companyName; 
   String mobile;
-
-  
   private Boolean isDeleted;
-
+  private String designation;
+  
+  
   public User() {
   }
 
@@ -114,6 +106,14 @@ public String getMobile() {
 
 public void setMobile(String mobile) {
 	this.mobile = mobile;
+}
+
+public String getDesignation() {
+	return designation;
+}
+
+public void setDesignation(String designation) {
+	this.designation = designation;
 }
 
 
